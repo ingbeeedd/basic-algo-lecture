d7a160265d9c3c37a69864882b6828f0888b77c7
ingbeeedd <57895472+ingbeeedd@users.noreply.github.com>
2022-01-07 18:25:47 +0900

    Update 1629.cpp

--- 0x0B/solutions/1629.cpp
+++ 0x0B/solutions/1629.cpp
@@ -1,23 +1,44 @@
-// Authored by : BaaaaaaaaaaarkingDog
+// Authored by : yeongjae8066
 // Co-authored by : -
-// http://boj.kr/a9f89b45ac624c2a8d13f27a01dd78d1
-#include <bits/stdc++.h>
-using namespace std;
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
 
-using ll = long long;
+long long func(long long a, long long b, long long c)
+{
+	if (b == 0) {
+		return 1;
+	}
+	else if (b == 1) {
+		return a % c;
+	}
 
-ll POW(ll a, ll b, ll m){
-  if(b==1) return a % m;
-  ll val = POW(a, b/2, m);
-  val = val * val % m;
-  if(b%2 == 0) return val;
-  return val * a % m;
+	long long l, r;
+	if (b % 2) { // odd
+		l = func(a, b / 2, c);
+		r = func(a, 1, c);
+		
+	}
+	else { // even
+		l = func(a, b / 2, c);
+		r = func(a, 0, c);
+	}
+	
+	return (((l * l) % c) * r) % c;
 }
 
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  ll a,b,c;
-  cin >> a >> b >> c;
-  cout << POW(a,b,c);
+int main()
+{
+	std::ios_base::sync_with_stdio(0);
+	std::cin.tie(0);
+	std::cout.tie(0);
+
+	freopen("data.txt", "r", stdin);
+	long long a, b, c;
+	
+	std::cin >> a >> b >> c;
+	long long answer = func(a, b, c);
+	std::cout << answer << std::endl;
+
+	return 0;
 }
